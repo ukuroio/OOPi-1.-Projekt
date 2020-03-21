f05da83717998c0dfdbd47457a0411eda87c94ff
ukuroio <56921091+ukuroio@users.noreply.github.com>
2020-03-21 16:26:01 +0200

    Liides, mis on vähem poolik

--- Projekt/src/Liides.java
+++ Projekt/src/Liides.java
@@ -2,34 +2,74 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Liides {
+
+    static boolean onAlgarv(int arv){
+        if (arv < 2){
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(arv) ; i++) {
+            if(arv % i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Millist tegevust soovid lasta programmil teha? Sisesta vastav arv." + "\n" + "1. Jaga" + "\n" + "2. Korruta" +
                 "\n" + "3. Liida" + "\n" + "4. Lahuta" + "\n" + "5. Leia polünoomide SÜT" + "\n" + "6. Leia polünoomide VÜK" +
-                "\n" + "7. Leia pöördarv");
+                "\n" + "7. Leia pöördarv jäägiklassis" + "\n" + "8. Astenda jäägiklassis");
         int tegevus = scan.nextInt();
         System.out.println("Valisid tegevuse " + tegevus);
         System.out.println("Sisesta moodul, mille järgi soovid jäägiklassikorpuse luua. Moodul peab olema algarv.");
         int moodul = scan.nextInt();
+
+        if(!onAlgarv(moodul)){
+            System.out.println("Moodul peab olema algarv. Palun sisesta uus arv.");
+            moodul = scan.nextInt();
+        }
+
         Jäägiklass Zn = new Jäägiklass(moodul);
         Zn.looTsükkel();
         Polünoomid moodul2 = new Polünoomid(moodul);
         System.out.println("Valisid mooduliks arvu " + moodul + ".");
-        System.out.println("Kas soovid tehet teostada: 1. jäägiklassikorpuses või 2. polünoomide klassis?");
-        int valik1 = scan.nextInt();
-        if (valik1 == 1) {
-            System.out.println("Sisesta täisarvud, millega soovid tehteid teha.");
-            int arv1 = scan.nextInt();
-            int arv2 = scan.nextInt();
 
-            if(tegevus==1){
-                System.out.println("Jagamise tulemus on " + Zn.jagamine(arv1, arv2));
+        System.out.println("Kas soovid tehet teostada: 1. jäägiklassis või 2. polünoomide klassis?");
+        int valik1 = scan.nextInt();
+
+        //Siin kontrolli kas kasutaja pani õigesti
+
+        if (valik1 == 1) {
+            if(tegevus==7){
+                System.out.println("Sisesta täisarv, mille pöördarvu soovid leida.");
+                int arv = scan.nextInt();
+                System.out.println("Arvu pöördarv on " + Zn.pöördarv(arv));
             }
 
-            if(tegevus == 3){
-                System.out.println("Liitmise tulemus on " + Zn.liitmine(arv1,arv2));
+            else {
+                System.out.println("Sisesta täisarvud, millega soovid tehteid teha.");
+                int arv1 = scan.nextInt();
+                int arv2 = scan.nextInt();
+
+                if (tegevus == 1) {
+                    System.out.println("Jagamise tulemus on " + Zn.jagamine(arv1, arv2));
+                }
+
+                if (tegevus == 2) {
+                    System.out.println("Korrutamise tulemus on " + Zn.korrutamine(arv1, arv2));
+                }
+
+                if (tegevus == 3) {
+                    System.out.println("Liitmise tulemus on " + Zn.liitmine(arv1, arv2));
+                }
+
+                if (tegevus == 4) {
+                    System.out.println("Lahutamise tulemus on " + Zn.lahutamine(arv1, arv2));
+                }
             }
         }
+
         if (valik1 == 2) {
             System.out.println("Sisesta esimese polünoomi liikmete arv");
             int n = scan.nextInt();
@@ -49,9 +89,30 @@ public class Liides {
             }
             System.out.println("Sisestasid järgmise polünoomi: " + Arrays.toString(polünoom2));
 
+            if(tegevus==1){
+                System.out.println("Polünoomide jagatis on " + Arrays.toString((moodul2.jaga(polünoom, polünoom2))));
+            }
+
+            if(tegevus==2){
+                System.out.println("Polünoomide korrutis on " + Arrays.toString(moodul2.korruta(polünoom,polünoom2)));
+            }
+
+            if(tegevus==3){
+                System.out.println("Polünoomide summa on " + Arrays.toString(moodul2.liida(polünoom,polünoom2)));
+            }
+
+            if(tegevus==4){
+                System.out.println("Polünoomide vahe on " + Arrays.toString(moodul2.lahuta(polünoom,polünoom2)));
+            }
+
             if(tegevus==5){
                 System.out.println("Polünoomide SÜT on " + Arrays.toString(moodul2.ilusaleKujule((moodul2.SÜT(polünoom, polünoom2)))));
             }
+
+            if(tegevus==6){
+                System.out.println("Polünoomide VÜK on " + Arrays.toString(moodul2.VÜK(polünoom, polünoom2)));
+            }
+
         }
     }
 }
